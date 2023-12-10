7458fbf443cc702fc7c35d1f29fb229635ea98aa
ZeeHatcher <zee.hatcher010@gmail.com>
2023-12-10 15:17:52 +0800

    feat: day 9

---
+++ day_9/solution_2.cpp
@@ -0,0 +1,68 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+long extrapolate(vector<int> history) {
+    bool all_zeroes = true;
+    for (int &h : history) {
+        if (h != 0) {
+            all_zeroes = false;
+            break;
+        }
+    }
+    if (all_zeroes) { return 0; }
+
+    vector<int> sub;
+
+    for (int i = 1; i < history.size(); i++) {
+        sub.push_back(history[i] - history[i-1]);
+    }
+
+    return history[0] - extrapolate(sub);
+}
+
+vector<int> parse_numbers(string line) {
+    vector<int> numbers;
+    int start = -1;
+    for (int i = 0; i < line.length(); i++) {
+        char c = line[i];
+        bool is_num_char = isdigit(c) || c == '-';
+        if (start == -1 && is_num_char) {
+            start = i;
+        }
+
+        if (start != -1 && !is_num_char) {
+            numbers.push_back(stoi(line.substr(start, i)));
+            start = -1;
+        }
+    }
+
+    numbers.push_back(stoi(line.substr(start)));
+
+    return numbers;
+}
+
+string get_line(fstream &file) {
+    char raw[128];
+    file.getline(raw, 128);
+    return string(raw);
+}
+
+int main() {
+    fstream file("input.txt");
+
+    int score = 0;
+    while (file.good()) {
+        string line = get_line(file);
+        vector<int> history = parse_numbers(line);
+        int forecast = extrapolate(history);
+        score += forecast;
+    }
+
+    cout << "Score: " << score << endl;
+
+    return 0;
+}
